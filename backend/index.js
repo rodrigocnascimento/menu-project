8f307cc51952220efb6c6e2333d82dfbeacefce7
Rodrigo Nascimento <rodrigo.nascimento@starpay.com.br>
2020-02-12 18:27:50 -0300

    adding express packages and creating start script

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+const { SERVER_PORT } = process.env
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+const testRoute = require("./routes/test")
+
+app.use("/", testRoute)
+
+app.listen(SERVER_PORT, () => console.log(`Server listening on SERVER_port ${SERVER_PORT}!`))
