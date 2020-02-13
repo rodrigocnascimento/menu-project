f85a0be6531ae502fcdec8c5fd3d3dfd7a3401d9
Rodrigo Nascimento <rodrigo.nascimento@starpay.com.br>
2020-02-13 12:41:36 -0300

    using react hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
+  const [testApiResponse, setTestApiResponse] = useState("")
+  const { env } = props
+  
+  useEffect(() => {
+    fetch(env.URI)
+      .then(async response => {
+        setTestApiResponse(await response.text())
+      })
+      .catch(err => err);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="App-intro">{testApiResponse}</p>
       </header>
     </div>
   );
