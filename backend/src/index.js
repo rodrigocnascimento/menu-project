2e9d6d2a0fcc917ec3f5c1e5c7e5a9decdc5359d
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2020-02-14 03:31:32 -0300

    app entrypoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,10 +10,12 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-const testRoute = require("./routes/test")
+const clientRoute = require("./routes/clients")
+const ordersRoute = require("./routes/orders")
+const nerdRoute = require("./routes/nerd")
 
-app.all("/", (req, res) => res.send(`default route`))
+app.use("/cliente", clientRoute)
+app.use("/pedido", ordersRoute)
+app.use("/nerdStats", nerdRoute)
 
-app.use("/test", testRoute)
-
-app.listen(SERVER_PORT, () => console.log(`Server listening on port ${SERVER_PORT}!`))
+app.listen(SERVER_PORT, console.log(`Server listening on port ${SERVER_PORT}!`))
