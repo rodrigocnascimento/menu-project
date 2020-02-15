7af8c492e3367e58b8aa32f61f042060e3460685
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2020-02-15 19:00:31 -0300

    updating curtomer relation <> order

--- backend/src/models/customer.js
+++ backend/src/models/customer.js
@@ -1,12 +1,16 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
+  
   const Customer = sequelize.define('Customer', {
     name: DataTypes.STRING,
     lastName: DataTypes.STRING,
     email: DataTypes.STRING
   }, {});
+
   Customer.associate = function(models) {
-    // associations can be defined here
+    const { Customer, Order } = models
+
+    Customer.hasMany(Order)
   };
   return Customer;
 };
