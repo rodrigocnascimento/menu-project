640b596ffdf2c9561e66cd79f2f5a80de7c6b46e
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2020-02-17 01:37:06 -0300

    startup refactor

--- start.sh
+++ start.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-mv ./frontend/env.example ./frontend/.env
-mv ./backend/env.example ./backend/.env 
-mv ./database/env.example ./database/.env 
+if [ -f ./frontend/env.example ]; then
+    mv ./frontend/env.example ./frontend/.env
+fi
+
+if [ -f ./backend/env.example ]; then
+    mv ./backend/env.example ./backend/.env 
+fi
+
+if [ -f ./database/env.example ]; then
+    mv ./database/env.example ./database/.env 
+fi
 
 sleep 2
 echo "Iniciando o projeto"
