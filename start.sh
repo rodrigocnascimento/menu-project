22cf6425fb008ffaad09949c60bf4043c7c56583
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2020-02-17 01:41:04 -0300

    avoiding losing files

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [ -f ./frontend/env.example ]; then
-    mv ./frontend/env.example ./frontend/.env
+    cp ./frontend/env.example ./frontend/.env
 fi
 
 if [ -f ./backend/env.example ]; then
-    mv ./backend/env.example ./backend/.env 
+    cp ./backend/env.example ./backend/.env 
 fi
 
 if [ -f ./database/env.example ]; then
-    mv ./database/env.example ./database/.env 
+    cp ./database/env.example ./database/.env 
 fi
 
 sleep 2
